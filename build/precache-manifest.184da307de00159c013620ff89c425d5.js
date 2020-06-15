d57bce1fcaaf8231fe11407e72d34a121af15f02
SamirAhma <ahmaddin1000@gmail.com>
2020-06-15 09:29:07 +0800

    fix index.html

---
+++ build/precache-manifest.184da307de00159c013620ff89c425d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d62ab355c1f7087e65ef21b03c7a89b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fb3a56177eb336cfd50",
+    "url": "/static/css/main.732afdec.chunk.css"
+  },
+  {
+    "revision": "e0f0dc00ae6ae6f602c3",
+    "url": "/static/js/2.e5f8debb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e5f8debb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fb3a56177eb336cfd50",
+    "url": "/static/js/main.cdb53211.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  }
+]);
