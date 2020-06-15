a745247b7c3724f2cf43f4f7ca169716a7a8398b
SamirAhma <ahmaddin1000@gmail.com>
2020-06-15 08:09:12 +0800

    fisr

--- todo/settings.py
+++ todo/settings.py
@@ -144,7 +144,7 @@ CORS_ORIGIN_WHITELIST = [
 
 
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS =[
       os.path.join(BASE_DIR, 'build/static')
